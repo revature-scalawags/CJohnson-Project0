5f82771efd1538fd9df5db73c8f6dea2d43dfb91
chris johnson <cjohn281@outlook.com>
2020-12-14 09:10:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Chord Library
+A simple guitar chord library application
+
+## Compile
+>sbt compile
+
+## Test
+>sbt test
+
+## Run
+>stb --error run
+
+# Requirements
+# Features
---
+++ src/main/scala/ChordLibrary.scala
@@ -0,0 +1,35 @@
+/** Simple Guitar Chord Library
+  * 
+  */
+object ChordLibrary extends App {
+
+    val file = getFile("GuitarChords.csv")
+    printContents(file)
+
+    /** Loads in a file
+      *
+      * @param fileName The name of the file
+      * @return the io.Source object created from the fileName
+      */
+    def getFile(fileName: String): io.Source = io.Source.fromFile(fileName)
+
+
+    /** Prints the entire contents of a CSV file to the command line
+      *
+      * @param file The CSV file to be printed
+      */
+    def printContents(file: io.Source): Unit = {
+      for (line <- file.getLines()) {
+        val col = line.split(";")
+        println(s"${col(0)} \t ${col(1)} \t ${col(2)} ${spacer(col(2))} ${col(3)} ${spacer(col(3))} ${col(4)}")
+      }
+    }
+
+
+    /** Used to format the data output
+      *
+      * @param text String cell data from the CSV file
+      * @return A string containing the appropriate number of tab characters
+      */
+    def spacer(text: String): String = if (text.length < 6) "\t\t" else "\t"
+}
