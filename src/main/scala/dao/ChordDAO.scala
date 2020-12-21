12b3d2da83dd6d3745a3cd9713a04ac36cf5bfdd
Chris Johnson <cjohn281@outlook.com>
2020-12-21 18:11:10 -0500

    Moved database connection values to utilities.DAOUtility trait

--- src/main/scala/dao/ChordDAO.scala
+++ src/main/scala/dao/ChordDAO.scala
@@ -1,19 +1,9 @@
 package dao
 
-import org.bson.codecs.configuration.CodecRegistries.{fromRegistries, fromProviders}
-import org.mongodb.scala.bson.codecs.Macros._
-import org.mongodb.scala.MongoClient
-import org.mongodb.scala.MongoCollection
 import org.mongodb.scala.Observable
 
-import model.Chord
-
 trait ChordDAO[T] {
-  val codecRegistry = fromRegistries(fromProviders(classOf[Chord]), MongoClient.DEFAULT_CODEC_REGISTRY)
-  val client = MongoClient()
-  val db = client.getDatabase("testdb").withCodecRegistry(codecRegistry)
-  val coll: MongoCollection[Chord] = db.getCollection("chords")
-
+  
   def storeResults(obs: Observable[T]): Seq[T]
   def getAll: Seq[T]
   def getRoot(root: String): Seq[T]
--- src/main/scala/dao/ChordDAOImpl.scala
+++ src/main/scala/dao/ChordDAOImpl.scala
@@ -6,12 +6,11 @@ import scala.concurrent.Await
 import scala.concurrent.duration.{Duration, SECONDS}
 
 import model.Chord
+import utilities.DAOUtility
 
-class ChordDAOImpl extends ChordDAO[Chord] {
+class ChordDAOImpl extends ChordDAO[Chord] with DAOUtility {
 
   def storeResults(obs: Observable[Chord]): Seq[Chord] = Await.result(obs.toFuture(), Duration(10, SECONDS))
-
   def getAll: Seq[Chord] = storeResults(coll.find())
-
   def getRoot(root: String): Seq[Chord] = storeResults(coll.find(equal("ROOT", root.capitalize)))
 }
