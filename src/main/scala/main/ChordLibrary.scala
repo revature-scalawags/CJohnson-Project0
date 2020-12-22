8d7bbb2d18313bc73a00c8eec19b9cd626054b9a
Chris Johnson <cjohn281@outlook.com>
2020-12-22 01:03:41 -0500

    Added functionality to search chords by containing notes, plus minor refactoring

--- src/main/scala/main/ChordLibrary.scala
+++ src/main/scala/main/ChordLibrary.scala
@@ -7,11 +7,12 @@ import utilities.FlagFunctions
 /** Simple Guitar Chord Library Application */
 object ChordLibrary extends App {
 
+  // Notify user if not arguments have been passed in
   if (args.length != 0) {
     parseArgs(args)
   } else FlagFunctions.invalidInputNotify()
 
-  
+ 
   /** Parses argument list
     * 
     * @param args argument list
@@ -19,7 +20,8 @@ object ChordLibrary extends App {
     */
   def parseArgs(args: Array[String]): Unit = args(0) match {
     case "-a" | "--all" => FlagFunctions.printAll()
-    case "-r" | "--root" => FlagFunctions.printByRoots(args)
+    case "-r" | "--root" => FlagFunctions.printByRoot(args)
+    case "-s" | "--search" => FlagFunctions.searchChords(args.slice(1, args.length).map(_.toUpperCase()))
     case "-h" | "--help" => FlagFunctions.printHelp()
     case _ => FlagFunctions.invalidInputNotify()
   }
